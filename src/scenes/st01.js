3b387e4c50b3fa9e27e408b3a7bd0c4b095f8388
tringcooler <tringcooler@users.noreply.github.com>
2020-01-03 02:09:50 +0800

    standard commit

--- src/scenes/st01.js
+++ src/scenes/st01.js
@@ -1,6 +1,8 @@
 define(function(require) {
     
-    const IMGS = (n, ex) => '../assets/img/' + n + (ex ? ex : '.png');
+    const 
+        ASSETS = n => '../assets/' + n;
+        IMGS = (n, ex) => ASSETS('img/' + n + (ex ? ex : '.png'));
 
     function preload() {
         this.load.spritesheet('player1', 
@@ -8,10 +10,16 @@ define(function(require) {
                 frameWidth: 72, frameHeight: 72,
             },
         );
+        this.load.image('bg_tiles', IMGS('mp01'));
+        this.load.tilemapTiledJSON('bg', ASSETS('map/st01.json'));
     }
 
     function create() {
-        //let logo = this.add.image(320, 240, 'logo');
+        let bg_map = this.make.tilemap({key: 'bg'});
+        let bg_tiles = bg_map.addTilesetImage('mp01', 'bg_tiles');
+        let bg_layer = bg_map.createStaticLayer('background', 'mp01',
+            this.cameras.main.centerX - bg_map.widthInPixels / 2,
+            this.cameras.main.centerY - bg_map.heightInPixels / 2);
     }
     
     function update() {
@@ -21,7 +29,7 @@ define(function(require) {
         key: 'st01',
         preload: preload,
         create: create,
-        update: update,
+        //update: update,
     };
     
 });
