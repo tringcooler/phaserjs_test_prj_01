0eacfef728ffeb1aa6c17e2016e7f1874e7dba1c
tringcooler <tringcooler@users.noreply.github.com>
2020-01-05 01:47:37 +0800

    Update entrance.js

--- src/entrance.js
+++ src/entrance.js
@@ -2,8 +2,7 @@ requirejs.config({
     baseUrl: 'src',
     paths: {
         jquery: '../lib/jquery-1.12.4.min',
-        //Phaser: '../lib/phaser.min',
-        Phaser: '//cdn.jsdelivr.net/npm/phaser@3.21.0/dist/phaser.min',
+        Phaser: location.hostname == '127.0.0.1' ? '../lib/phaser.min' : '//cdn.jsdelivr.net/npm/phaser@3.21.0/dist/phaser.min',
         core: 'core',
         graph: 'graph',
         scenes: 'scenes',
