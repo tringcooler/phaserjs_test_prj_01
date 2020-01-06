da2949380dba805efadc8758ba99048e531e1599
tringcooler <tringcooler@users.noreply.github.com>
2020-01-07 01:44:43 +0800

    Update st02.js

--- src/scenes/st02.js
+++ src/scenes/st02.js
@@ -26,10 +26,10 @@ define(function(require) {
             delay: 1,
         });*/
         
-        const thr = 0.1;
+        const thr = 0.2;
         spc_met.addMarker(metro_marker);
         spc_met.play('metro', {
-            delay: .749,// - thr / 2,
+            delay: .749 - thr / 2,
             mute: true,
         });
         let trigg_time = 0;
@@ -38,14 +38,14 @@ define(function(require) {
         });
         this.input.on('pointerdown', p => {
             let c_time = this.time.now / 1000;
-            let delt_time = (c_time - trigg_time);
-            delt_time = Math.min(delt_time, 0.5 - delt_time);
+            let delt_time = c_time - trigg_time;
+            //delt_time = Math.min(delt_time, 0.5 - delt_time);
             let t = false;
             if(delt_time < thr) {
-                tac_r.play();
+                tac_r.play({delay: 0});
                 t = true;
             }
-            console.log(t ? 'o' : 'x', delt_time.toFixed(2), c_time.toFixed(2), trigg_time.toFixed(2));
+            console.log(t ? 'o' : 'x', (delt_time - thr / 2).toFixed(2), c_time.toFixed(2), trigg_time.toFixed(2));
         });
     }
     
