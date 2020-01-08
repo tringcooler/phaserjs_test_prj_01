15903cef179149e7dbb6cd71061e3e38762b6928
tringcooler <tringcooler@users.noreply.github.com>
2020-01-08 21:59:20 +0800

    Update st03.js

--- src/scenes/st03.js
+++ src/scenes/st03.js
@@ -69,45 +69,72 @@ define(function(require) {
         constructor(scene, frames, pos, cfg = ground_cfg) {
             this.scene = scene
             this.cfg = cfg;
-            this._calc_frame_shape(frames);
-            this.prev_frames = null;
             let width = cfg.tile_size[0] * cfg.len
             this.size = [width, cfg.tile_size[1] * frames.length];
             this.layer = new c_layer(this.scene, pos, [width, cfg.lim_h]);
+            let finfo = this._calc_finfo(frames);
+            this._fill_one(0, false, null, finfo);
             this._fill();
         }
         
-        _calc_frame_shape(frames) {
+        _calc_finfo(sframes) {
             let _a = v => (v instanceof Array) ? v : [v];
-            this.frames = [];
-            this.fshape = [0, 0, 0];
-            frames = _a(frames);
-            for(let frames_row of frames) {
+            let frames = [];
+            let fshape = [0, 0, 0];
+            sframes = _a(sframes);
+            for(let frames_row of sframes) {
                 frames_row = _a(frames_row);
-                this.frames.push(frames_row);
-                this.fshape[1] ++;
-                this.fshape[0] = Math.max(this.fshape[0], frames_row.length);
-                this.fshape[2] += frames_row.length;
+                frames.push(frames_row);
+                fshape[1] ++;
+                fshape[0] = Math.max(fshape[0], frames_row.length);
+                fshape[2] += frames_row.length;
             }
+            return {frames: frames, fshape: fshape};
         }
         
-        _fill_one(px, left = false, tile = null) {
-            for(let iy = 0; iy < this.fshape[1]; iy ++) {
-                let y = (iy - (this.fshape[1] - 1) / 2) * this.cfg.tile_size[1];
-                let frames_row = this.frames[iy];
+        _get_tiles(idx) {
+            return this.layer.co.list.slice(
+                idx * this.fshape[2]).slice(0, this.fshape[2]);
+        }
+        
+        _get_tile(tile) {
+            if(typeof tile == 'number') tile = this._get_tiles(tile);
+            if(tile instanceof Array) tile = tile[0];
+            return tile;
+        }
+        
+        _get_x(tile) {
+            tile = this._get_tile(tile);
+            return tile.x + tile._ground_off_x;
+        }
+        
+        _get_finfo(tile) {
+            tile = this._get_tile(tile);
+            return tile._ground_finfo;
+        }
+        
+        _fill_one(px, left = false, tiles = null, finfo = null) {
+            if(!finfo) {
+                finfo = this._get_finfo(left ? 0 : -1);
+            }
+            let tiles_idx = 0;
+            for(let iy = 0; iy < finfo.fshape[1]; iy ++) {
+                let y = (iy - (finfo.fshape[1] - 1) / 2) * this.cfg.tile_size[1];
+                let frames_row = finfo.frames[iy];
                 if(!frames_row) break;
-                for(let ix = 0; ix < this.fshape[0]; ix ++) {
-                    let x = (ix - (this.fshape[0] - 1) / 2) * this.cfg.tile_size[0];
+                for(let ix = 0; ix < finfo.fshape[0]; ix ++) {
+                    let x = (ix - (finfo.fshape[0] - 1) / 2) * this.cfg.tile_size[0];
                     let frame = frames_row[ix];
                     if(!frame) break;
-                    let sp = tile;
-                    if(sp) {
+                    let sp;
+                    if(tiles) {
+                        sp = tiles[tiles_idx ++];
                         sp.setPosition(px, y);
                     } else {
                         sp = this.scene.add.sprite(px + x, y, this.cfg.tiles, frame);
                     }
                     sp._ground_off_x = x;
-                    sp._ground_frames = this.frames;
+                    sp._ground_finfo = finfo;
                     if(left) {
                         this.layer.co.addAt(sp, 0);
                     } else {
@@ -117,20 +144,38 @@ define(function(require) {
             }
         }
         
-        _get_tiles(idx) {
-            return this.layer.co.list.slice(
-                idx * this.fshape[2]).slice(0, this.fshape[2]);
+        _filled_status() {
+            let top_l = this._get_tiles(0);
+            let top_r = this._get_tiles(-1);
+            let _cs = (lx, rx) => px =>(px > rx) ? 1 : (px < lx) ? -1 : 0;
+            let top_n = (this.cfg.len - 1) / 2,
+                t_w = this.cfg.tile_size[0];
+            let [x_l, x_r] = [this._get_x(top_l), this._get_x(top_r)];
+            let sta_l = - _cs(- (top_n + 1) * t_w, - top_n * t_w)(x_l);
+            let sta_r = _cs(top_n * t_w, (top_n + 1) * t_w)(x_r);
+            return [[top_l, top_r], [sta_l, sta_r], [x_l, x_r]];
         }
         
-        _get_x(tile) {
-            if(typeof tile == 'number') tile = this._get_tiles(tile)[0];
-            return tile.x + tile._ground_off_x;
-        }
-        
-        _fill() {
-            for(let x = -200; x < 200; x += 24 * this.fshape[0]) {
-                this._fill_one(x);
+        _fill(left = false, finfo = null) {
+            let hd_idx = 1,
+                tl_idx = 0;
+            if(left) {
+                hd_idx = 0;
+                tl_idx = 1;
             }
+            let [top_tiles, top_status, top_x] = this._filled_status();
+            if(top_status[hd_idx] <= 0) return;
+            if(!finfo) {
+                finfo = this._get_finfo(top_tiles[hd_idx]);
+            }
+            let tiles = null;
+            if(top_status[tl_idx] < 0
+                && finfo === this._get_finfo(top_tiles[tl_idx])) {
+                tiles = top_tiles[tl_idx];
+            }
+            let step_x = 24 * finfo.fshape[0];
+            if(left) step_x = - step_x;
+            this._fill_one(top_x[hd_idx] + step_x, left, tiles, finfo);
         }
         
     }
@@ -143,7 +188,8 @@ define(function(require) {
             angle: 360,
             duration: 6000,
             yoyo: false,
-            repeat: -1
+            repeat: -1,
+            //onRepeat: () => console.log('.'),
         });
     }
     
